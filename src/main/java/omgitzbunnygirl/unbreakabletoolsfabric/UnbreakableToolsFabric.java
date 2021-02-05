568e5aeea382d3a0a25885284b44a268a80f721a
WUT DA HACK <70644502+WUTDAHACK@users.noreply.github.com>
2021-02-05 09:52:19 +0000

    1.15.2 update

--- src/main/java/omgitzbunnygirl/unbreakabletoolsfabric/UnbreakableToolsFabric.java
+++ src/main/java/omgitzbunnygirl/unbreakabletoolsfabric/UnbreakableToolsFabric.java
@@ -2,7 +2,12 @@ package omgitzbunnygirl.unbreakabletoolsfabric;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.item.*;
+import net.minecraft.item.ToolItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolMaterials;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import omgitzbunnygirl.unbreakabletoolsfabric.item.*;
